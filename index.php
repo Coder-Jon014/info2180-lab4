d63cffa05a3f5aa0537f274f11bd0fd8b65f0eaf
Coder-Jon014 <jon.coombs14@gmail.com>
2021-10-31 22:22:16 -0500

    Updated Files

--- index.php
+++ index.php
@@ -8,18 +8,26 @@
 </head>
 <body>
     <div class="container">
-        <h1>Avengers Character Search</h1>
+        <header>
+          <h1>Avengers Character Search</h1>
+        </header>
+        
+        <section>
+          <div class="search">
+            <input type="searchBar" id="searchBar" name="searchBar" placeholder="e.g. Captain America or Steve Rogers">
+          </div>
+    
+            <button type="button" id="searchButton" class="btn">Search</button>
 
-        <div class="search">
-        <input type="searchBar" id="searchBar" name="searchBar" placeholder="e.g. Captain America or Steve Rogers">
-        </div>
+            <h3>Result</h3>
+            <div id="superheroes"></div>
+        </section>
 
-        <button type="button" id="searchButton" class="btn">Search</button>
 
-        <h2>Superheroes</h2>
-      <blockquote id="superheroes"></blockquote>
+
     </div>
 </body>
+
 
 
 
--- script.js
+++ script.js
@@ -6,20 +6,22 @@ window.onload = function(){
         elements.preventDefault();
 
         httpsRequest = new XMLHttpRequest();
+        var searchBar = document.querySelector('#searchBar').value;
 
-        var url = "superheroes.php";
+        var url = "superheroes.php?query=";
         httpsRequest.onreadystatechange = superHeroes;
-        httpsRequest.open('GET', url);
+        httpsRequest.open('GET', url+searchBar, true);
         httpsRequest.send();
+        console.log(searchBar);
     });
 
     function superHeroes(){
         if(httpsRequest.readyState === XMLHttpRequest.DONE){
             if(httpsRequest.status === 200){
                 var response = httpsRequest.responseText;
+                console.log(response);
                 var superhero = document.querySelector('#superheroes');
                 superhero.innerHTML = response;
-                alert(response);
             }else{
                 alert("There was a problem")
             }
